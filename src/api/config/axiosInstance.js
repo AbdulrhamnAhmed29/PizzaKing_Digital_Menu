2062b778a77943f1fd29bcdb670e3e849598e87c
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2026-06-21 16:41:03 +0300

    change base url for new server becuse the backend became production

--- src/api/config/axiosInstance.js
+++ src/api/config/axiosInstance.js
@@ -8,7 +8,7 @@ const apiUrl = process.env.REACT_APP_API_URL;
 // Create axios instance with default config
 
 const axiosInstance = axios.create({
-    baseURL: apiUrl || 'http://localhost:1337/api/',
+    baseURL: apiUrl ,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
